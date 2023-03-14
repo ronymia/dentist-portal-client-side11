999d0f193076fbf2c74f4111454a1c75f9dfa455
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-03-14 13:15:10 +0600

    project init

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../pages/Shared/Header/Header';
+
+const MainLayout = () => {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
+
+export default MainLayout;
