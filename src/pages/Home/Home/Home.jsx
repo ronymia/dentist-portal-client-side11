5afa4e1af097814c3961f21de2ce9402f68a253b
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-03-17 01:20:42 +0600

    make appointment

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import InfoCards from '../InfoCards/InfoCards';
+import MakeAppointment from '../MakeAppointment/MakeAppointment';
 import Services from '../Services/Services';
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
                <Banner />
                <InfoCards />
                <Services />
+               <MakeAppointment />
           </div>
      );
 }
---
+++ src/pages/Home/MakeAppointment/MakeAppointment.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import doctor from '../../../assets/images/doctor-small.png'
+import appointment from '../../../assets/images/appointment.png'
+
+const MakeAppointment = () => {
+    return (
+        <section className='mt-32'
+            style={{
+                background: `url(${appointment})`
+            }}
+        >
+            <div className="hero">
+                <div className="hero-content flex-col lg:flex-row">
+                    <img src={doctor}
+                        alt="doctor"
+                        className="-mt-32 hidden  md:block lg:w-1/2 rounded-lg shadow-2xl bg-transparent" />
+                    <div>
+                        <h4 className='text-lg text-secondary font-bold'>Appointment</h4>
+                        <h1 className=" text-white text-4xl font-semibold">Make an Appointment Today</h1>
+                        <p className="text-white py-6 text-[15px]">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                        <button type='button'
+                            className="btn bg-gradient-to-r from-primary to-secondary text-white rounded-lg border-0"
+                        >Appointment</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default MakeAppointment;
