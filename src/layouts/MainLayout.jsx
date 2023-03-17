67b450bb0e27fa23ac88441f055c6137fae30422
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-03-17 11:49:43 +0600

    footer

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from '../pages/Shared/Header/Header';
+import { Footer, Header } from '../pages/Shared';
 
 const MainLayout = () => {
     return (
         <>
             <Header />
             <Outlet />
+            <Footer />
         </>
     )
 }
