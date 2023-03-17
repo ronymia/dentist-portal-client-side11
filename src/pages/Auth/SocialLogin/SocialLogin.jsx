75f0fdf083fe6a6986cbc0b44083e605c1433ce1
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-03-17 14:59:49 +0600

    social login

---
+++ src/pages/Auth/SocialLogin/SocialLogin.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SocialLogin() {
+     return (
+          <>
+               <div className="divider text-dark">OR</div>
+               <button className='btn btn-outline w-full text-dark border-dark hover:text-white hover:bg-dark'>CONTINUE WITH GOOGLE</button>
+          </>
+     );
+}
