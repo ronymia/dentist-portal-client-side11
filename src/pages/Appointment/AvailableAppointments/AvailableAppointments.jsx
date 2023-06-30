2711d4afb5e8fc0390cc532d3f7510c5108f3f9e
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-06-30 22:26:06 +0600

    fixed

--- src/pages/Appointment/AvailableAppointments/AvailableAppointments.jsx
+++ src/pages/Appointment/AvailableAppointments/AvailableAppointments.jsx
@@ -35,7 +35,7 @@ export default function AvailableAppointments({ selectedDate }) {
                     setTreatment={setTreatment}
                 ></AppointmentOption>)}
             </div>
-            {/*   */}
+
             {
                 treatment &&
                 <BookingModal
