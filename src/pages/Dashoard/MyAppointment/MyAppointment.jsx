558665214311bab15014760986139a7cf9152166
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-10-01 22:53:34 +0600

    table integration

--- src/pages/Dashoard/MyAppointment/MyAppointment.jsx
+++ src/pages/Dashoard/MyAppointment/MyAppointment.jsx
@@ -7,26 +7,19 @@ export default function MyAppointment() {
                     <h2 className='text-black text-2xl'>My Appointment</h2>
                     <button type="button">click</button>
                </div>
-               <div className="mt-6">
-                    <div className="h-12 bg-[#E6E6E6] uppercase flex flex-row w-full items-center justify-center text-sm font-semibold text-black rounded-t-xl">
-                         <h4 className='w-[10%]'></h4>
-                         <h4 className='w-[30%]'>name</h4>
-                         <h4 className='w-[30%]'>service</h4>
-                         <h4 className='w-[30%]'>time</h4>
-                    </div>
-                    <div className="h-12 bg-white uppercase flex flex-row w-full items-center justify-center text-sm font-semibold text-black" >
-                         <h4 className='w-[10%] block text-center'>1</h4>
-                         <h4 className='w-[30%]'>John Doe</h4>
-                         <h4 className='w-[30%]'>Teeth Orthodontics</h4>
-                         <h4 className='w-[30%]'>	08.30 AM - 09.00 AM</h4>
-                    </div>
-                    <div className="h-12 bg-white uppercase flex flex-row w-full items-center justify-center text-sm font-semibold text-black" >
-                         <h4 className='w-[10%] block text-center'>2</h4>
-                         <h4 className='w-[30%]'>John Doe</h4>
-                         <h4 className='w-[30%]'>Teeth Orthodontics</h4>
-                         <h4 className='w-[30%]'>	08.30 AM - 09.00 AM</h4>
-                    </div>
-               </div>
+
+               <table className='w-full mt-5'>
+                    <tr className='h-12 bg-[#E6E6E6] uppercase text-sm font-semibold text-black rounded-t-xl'>
+                         <th>name</th>
+                         <th>service</th>
+                         <th>time</th>
+                    </tr>
+                    <tr className='text-center h-12 bg-white text-black'>
+                         <td>John Doe</td>
+                         <td>Teeth Orthodontics</td>
+                         <td>08.30 AM - 09.00 AM</td>
+                    </tr>
+               </table>
           </div>
      )
 }
